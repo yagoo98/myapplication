ae1fcefccbf89bbbd374380f02ea516c3cf5c036
andrew <AndrewChu@soetek.com.tw>
2024-08-06 14:52:51 +0800

    initial commit

---
+++ app/src/main/java/com/example/myapplication/Hello.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+class Hello {
+}
+//Gradle自動編譯到下列路徑:
+//C:\Users\andrew\AndroidStudioProjects\MyApplication\app\build\tmp\kotlin-classes\debug\com\example\myapplication
+//first label,kotlin 的main方法可以不用放在class裡面
+fun main() {
+    val c = "X"
+    val t = 10
+    println("Hello Kotlin!")
+}
