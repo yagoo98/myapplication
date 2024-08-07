a0bbf8a43a6f98e08f49c578fcbb69ef4ef61a90
andrew <AndrewChu@soetek.com.tw>
2024-08-07 17:56:37 +0800

    hw1

--- app/src/main/java/com/example/myapplication/Guessing.kt
+++ app/src/main/java/com/example/myapplication/Guessing.kt
@@ -1,18 +1,21 @@
 package com.example.myapplication
 
 fun main() {
-    val secret = 7
+    val question = "Please enter a number"
+    val secret = (1..10).random()
     var num = 0
+    var lowerBound = 1
+    var upperBound = 10
 
     while (num != secret) {
-        print("Please enter a number(1-10): ")
+        println("$question($lowerBound-$upperBound): ")
         val input = readlnOrNull()
         num = input?.toIntOrNull() ?: 0
-        println("The number you entered: $num")
+        if (num < 1 || num > 10) continue
         if (num < secret) {
-            println("Bigger")
+            lowerBound = num
         } else if (num > secret) {
-            println("Smaller")
+            upperBound = num
         } else {
             println("you got it!")
         }
